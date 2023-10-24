f5d591c5779131394f2dcb321ae997e8fae369dc
Igor Mikhailov <evgenyevich17@yandex.ru>
2023-10-24 23:12:55 +0700

    add post

--- src/components/profile/post.jsx
+++ src/components/profile/post.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import s from "../../css/modules/profile/post.module.css";
 
 const Post = () => {
+
+    let posts = [
+        {id: 1, message: "Hello! What's up man?", likesCount: 12},
+        {id: 2, message: "Don't missing me", likesCount: 6},        
+        {id: 3, message: "I'm going to the beach", likesCount: 34},
+        {id: 4, message: "Hello, I'm at home", likesCount: 58},
+        {id: 5, message: "Recca's amaing", likesCount: 8},
+    ]
+
+    let postEl = posts.map(p => <Post message={p.message} likesCount={p.likesCount} id={p.id}></Post>)
+
+
     return (
         <div className={s.post}>
             <h3 className={s.post__title}>
@@ -14,6 +26,9 @@ const Post = () => {
                 <button>
                     <span>add post</span>
                 </button>
+            </div>
+            <div className={s.post__posts}>
+                {postEl}
             </div>
         </div>
     )
